0d6db9b619227b0608e81dd0e2806378fc69d6fc
ruihu51 <ruihu.cufe@outlook.com>
2023-04-12 10:30:32 -0400

    simulation 3 update

--- simulation_scenario3.R
+++ simulation_scenario3.R
@@ -84,8 +84,8 @@ theta.hat.obs <- fit.obs$coefficients[1]
 theta.hat.obs.se <- sqrt(fit.obs$var[1,1])
 
 # only unmeasured confounding adjustment
-alpha <- -0.5
-beta <- -0.5
+alpha <- 0.5
+beta <- 0.5
 X <- cbind(sim.data$X1, sim.data$X2, sim.data$X3.star)
 A <- as.numeric(sim.data$A)-1
 fit.stoEM_reg <- survSensitivity(sim.data$Y, sim.data$D, A, X, "stoEM_reg",
@@ -93,6 +93,9 @@ fit.stoEM_reg <- survSensitivity(sim.data$Y, sim.data$D, A, X, "stoEM_reg",
                                  B = 5)
 theta.hat.stoEM_reg <- fit.stoEM_reg$tau1
 theta.hat.stoEM_reg.se <- fit.stoEM_reg$tau1.se
+
+#
+X <- cbind(sim.data$X1, sim.data$X2, sim.data$X3.star.cont)
 
 # only residual confounding adjustment
 sim.data$X3.star.cont <- as.numeric(sim.data$X3.star)-1
@@ -102,15 +105,38 @@ fit.simex.1 <- simex(naive.model.continuous, measurement.error = 0.8,
 theta.hat.simex.1 <- fit.simex.1$coefficients[1]
 theta.hat.simex.se.1 <- sqrt(fit.simex.1$variance.jackknife[1,1])
 
-alpha <- -0.5
-beta <- -0.5
+# adjusted both
+# source("https://raw.githubusercontent.com/Rong0707/survSens/master/R/SimulateU_surv.R")
+sim.data$Usim <- SimulateU_surv(sim.data$Y, sim.data$D, A, X, zetat = beta, zetaz = alpha, theta = 0.5, offset = TRUE)$U 
+# SimulateU_surv include iter=20 but not B=5
+naive.model.U <- coxph(Surv(Y, D) ~ A + X1 + X2 + X3.star.cont + Usim, data=sim.data, model = TRUE) # simex cannot take offset
+fit.simex.U.3 <- simex(naive.model.U, measurement.error = 0.8, 
+                     SIMEXvariable = c("X3.star.cont"), asymptotic = FALSE)
+
+# add B
+B <- 5
+theta.hat.simex.UB.3 <- c()
+for (i in 1:B){
+  sim.data$Usim <- SimulateU_surv(sim.data$Y, sim.data$D, A, X, zetat = beta, zetaz = alpha, theta = 0.5, offset = TRUE)$U 
+  # SimulateU_surv include iter=20 but not B=5
+  naive.model.U <- coxph(Surv(Y, D) ~ A + X1 + X2 + X3.star.cont + Usim, data=sim.data, model = TRUE) # simex cannot take offset
+  fit.simex.U.3 <- simex(naive.model.U, measurement.error = 0.8, 
+                         SIMEXvariable = c("X3.star.cont"), asymptotic = FALSE)
+  theta.hat.simex.UB.3[i] <- fit.simex.U.3$coefficients[1]
+}
+############
+alpha <- 0.5
+beta <- 0.5
 theta <- 1
 p_ij.gen <- p_ij.5
+seed.data <- sim.3.05.05.1.5.2500.pa
 ret <- data.frame()
 for (n in 2500) {
   for (j in 1:500) {
-    
-    seed <- sample(1e3:1e8, 1)
+    seed <- seed.data[seed.data$j==j,"seed"]
+    if (identical(seed, integer(0))){
+      seed <- sample(1e3:1e8, 1)
+    }
     set.seed(seed)
     cat(n, j, seed, '\n')
     
@@ -124,46 +150,53 @@ for (n in 2500) {
                                                   eta_t3=c(0,-0.2,-0.4,-0.6,-0.8,-1)))
     
     try({
-      # true
-      fit.true <- coxph(Surv(Y, D) ~ A + X1 + X2 + X3 + U, data=sim.data)
-      theta.hat.true <- fit.true$coefficients[1]
-      theta.hat.true.se <- sqrt(fit.true$var[1,1])
-      
-      # observed adjusted causal hazard ratio - no unmeasured & residual confounding adjustment
-      fit.obs <- coxph(Surv(Y, D) ~ A + X1 + X2 + X3.star, data=sim.data)
-      theta.hat.obs <- fit.obs$coefficients[1]
-      theta.hat.obs.se <- sqrt(fit.obs$var[1,1])
-      
-      # only unmeasured confounding adjustment
-      X <- cbind(sim.data$X1, sim.data$X2, sim.data$X3.star)
-      A <- as.numeric(sim.data$A)-1
-      fit.stoEM_reg <- survSensitivity(sim.data$Y, sim.data$D, A, X, "stoEM_reg",
-                                       zetaT = beta, zetaZ = alpha,
-                                       B = 5)
-      theta.hat.stoEM_reg <- fit.stoEM_reg$tau1
-      theta.hat.stoEM_reg.se <- fit.stoEM_reg$tau1.se
-      
-      # only residual confounding adjustment
+      # # true
+      # fit.true <- coxph(Surv(Y, D) ~ A + X1 + X2 + X3 + U, data=sim.data)
+      # theta.hat.true <- fit.true$coefficients[1]
+      # theta.hat.true.se <- sqrt(fit.true$var[1,1])
+      # 
+      # # observed adjusted causal hazard ratio - no unmeasured & residual confounding adjustment
+      # fit.obs <- coxph(Surv(Y, D) ~ A + X1 + X2 + X3.star, data=sim.data)
+      # theta.hat.obs <- fit.obs$coefficients[1]
+      # theta.hat.obs.se <- sqrt(fit.obs$var[1,1])
+      # 
+      # # only unmeasured confounding adjustment
+      # X <- cbind(sim.data$X1, sim.data$X2, sim.data$X3.star)
+      # A <- as.numeric(sim.data$A)-1
+      # fit.stoEM_reg <- survSensitivity(sim.data$Y, sim.data$D, A, X, "stoEM_reg",
+      #                                  zetaT = beta, zetaZ = alpha,
+      #                                  B = 5)
+      # theta.hat.stoEM_reg <- fit.stoEM_reg$tau1
+      # theta.hat.stoEM_reg.se <- fit.stoEM_reg$tau1.se
+      # 
+      # # only residual confounding adjustment
       sim.data$X3.star.cont <- as.numeric(sim.data$X3.star)-1
-      naive.model.continuous <- coxph(Surv(Y, D) ~ A + X1 + X2 + X3.star.cont, data=sim.data, model = TRUE)
-      fit.simex.3 <- simex(naive.model.continuous, measurement.error = 0.8, 
-                           SIMEXvariable = c("X3.star.cont"), asymptotic = FALSE)
-      theta.hat.simex.3 <- fit.simex.3$coefficients[1]
-      theta.hat.simex.se.3 <- sqrt(fit.simex.3$variance.jackknife[1,1])
+      # naive.model.continuous <- coxph(Surv(Y, D) ~ A + X1 + X2 + X3.star.cont, data=sim.data, model = TRUE)
+      # fit.simex.3 <- simex(naive.model.continuous, measurement.error = 0.8, 
+      #                      SIMEXvariable = c("X3.star.cont"), asymptotic = FALSE)
+      # theta.hat.simex.3 <- fit.simex.3$coefficients[1]
+      # theta.hat.simex.se.3 <- sqrt(fit.simex.3$variance.jackknife[1,1])
+      
+      # adjusted both
+      sim.data$Usim <- SimulateU_surv(sim.data$Y, sim.data$D, A, X, zetat = beta, zetaz = alpha, theta = 0.5, offset = TRUE)$U 
+      # SimulateU_surv include iter=20 but not B=5
+      naive.model.U <- coxph(Surv(Y, D) ~ A + X1 + X2 + X3.star.cont + Usim, data=sim.data, model = TRUE) # simex cannot take offset
+      fit.simex.U.3 <- simex(naive.model.U, measurement.error = 0.8, 
+                             SIMEXvariable = c("X3.star.cont"), asymptotic = FALSE)
+      theta.hat.simex.U.3 <- fit.simex.U.3$coefficients[1]
+      theta.hat.simex.se.U.3 <- sqrt(fit.simex.U.3$variance.jackknife[1,1])
+      
+      # 
       
       if (length(ret)>0){
-        ret.tmp <- data.frame(true = theta.hat.true, true.se = theta.hat.true.se,
-                              observed = theta.hat.obs, observed.se = theta.hat.obs.se, 
-                              stoEM_reg = theta.hat.stoEM_reg, stoEM_reg.se = theta.hat.stoEM_reg.se, 
-                              simex.3 = theta.hat.simex.3, simex.se.3 = theta.hat.simex.se.3,
+        ret.tmp <- data.frame(
+                              simex.U.3 = theta.hat.simex.U.3, simex.se.U.3 = theta.hat.simex.se.U.3,
                               n=n, j=j, seed=seed)
         
         ret <- rbind(ret, ret.tmp)
       } else {
-        ret <- data.frame(true = theta.hat.true, true.se = theta.hat.true.se,
-                          observed = theta.hat.obs, observed.se = theta.hat.obs.se, 
-                          stoEM_reg = theta.hat.stoEM_reg, stoEM_reg.se = theta.hat.stoEM_reg.se, 
-                          simex.3 = theta.hat.simex.3, simex.se.3 = theta.hat.simex.se.3,
+        ret <- data.frame( 
+                          simex.U.3 = theta.hat.simex.U.3, simex.se.U.3 = theta.hat.simex.se.U.3,
                           n=n, j=j, seed=seed)
       }
     })
@@ -171,7 +204,53 @@ for (n in 2500) {
   }
 }
 
+library(parallel)
+
+tm <- system.time(
+  ret <- mclapply(1:500, sim.3.hybrid, n_range=2500,
+                                      seed.data=sim.3.05.05.1.5.2500.pa,
+                                      alpha=0.1, beta=0.1,
+                                      theta=1, p_ij=p_ij.5, B=1,
+                                      mc.cores = 4)
+)
+sim.3.hybrid(n_range=2500, j_range=1:2, seed.data=sim.3.05.05.1.5.2500.pa,
+             alpha=0.1, beta=0.1,
+             theta=1, p_ij=p_ij.5, B=1)
+
+ret
+
 # save data format
 # sim.{scenario}.{alpha}.{beta}.{theta}.{mis_prob}.{n}.{project}
 sim.3.05.05.1.5.2500.pa <- ret
 save(sim.3.05.05.1.5.2500.pa, file="../sim.data/sim.3.05.05.1.5.2500.pa.RData")
+
+# result
+ret1 <- do.call(rbind.data.frame, ret)
+sim.3.05.05.4.5.2500.pa <- sim.3.05.05.1.5.2500.pa
+head(sim.3.05.05.4.5.2500.pa)
+ret.4.est <- sim.3.05.05.4.5.2500.pa %>%
+  gather(key='type', value='est', c("true", "observed", 
+                                    "stoEM_reg", "simex.3", "simex.U.3", "simex.UB.3")) %>%
+  filter(type %in% c("true", "observed", 
+                     "stoEM_reg", "simex.3", "simex.U.3", "simex.UB.3")) %>%
+  mutate(bias=est-(1))
+
+ret.4.est %>% group_by(type) %>%
+  dplyr::summarise(est.mean = mean(est),
+            bias.mean = mean(abs(bias)))
+
+ret.4.est$type
+library(plyr)
+
+detach_package("plyr", TRUE)
+sim.3.05.05.4.5.2500.pa <- join(sim.3.05.05.4.5.2500.pa, ret1, by=c("n", "j", "seed"), type="inner")
+head(sim.3.05.05.4.5.2500.pa)
+
+# the range of alpha and beta
+sim.data$X1.cont <- as.numeric(sim.data$X1)-1
+sim.data$X2.cont <- as.numeric(sim.data$X2)-1
+sim.data$X3.cont <- as.numeric(sim.data$X3)-1
+coxph(Surv(Y, D) ~ A + X1.cont + X2.cont + X3.cont, data=sim.data, model = TRUE)
+
+# alpha, beta = 0.1, 0.1
+
