1c3c2054a51057b0eef430f2ef19d6440ec280d5
ruihu51 <ruihu.cufe@outlook.com>
2023-09-22 11:56:34 -0400

    update health

--- data_application_pipeline.R
+++ data_application_pipeline.R
@@ -29,6 +29,19 @@ trans_age <- function(x){
   }
 }
 aarp_data$ENTRY_AGE1 <- unlist(lapply(aarp_data$ENTRY_AGE, trans_age))
+# HEALTH
+trans_health <- function(x){
+  if (x<=2) { 
+    return(1)
+  } else if (x<=3) {
+    return(2)
+  } else if  (x<=5) {
+    return(3)
+  } else {
+    return(4)
+  }
+}
+aarp_data$HEALTH1 <- unlist(lapply(aarp_data$HEALTH, trans_health))
 
 # smoking behavior
 # SMOKE_DOSE
@@ -150,15 +163,15 @@ table(aarp_data$RF_PHYS_MODVIG_CURR1) # no NA
 # naive model - lung_mort
 data1 <- aarp_data[aarp_data$RF_PHYS_MODVIG_CURR1!=9,]
 # data2 <- data1[!is.na(data1$FUQ_SMOKE_STOP),]
-data2 <- data1
+data2 <- data1[!is.na(data1$FL_PROXY),]
 
 fit.naive.lungcancer.1 <- coxph(Surv(PERSONYRS, LUNG_MORT1) ~ factor(RF_PHYS_MODVIG_CURR1) 
-                   + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH)
+                   + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH1)
                    + factor(BMI_CUR1) + factor(HEI2015_TOTAL_SCORE1) + factor(MPED_A_BEV_NOFOOD1)
                    + factor(SMOKE_DOSE), data=data1)
 
 fit.naive.lungcancer.2 <- coxph(Surv(PERSONYRS, LUNG_MORT1) ~ factor(RF_PHYS_MODVIG_CURR1) 
-                    + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH)
+                    + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH1)
                     + factor(BMI_CUR1) + factor(HEI2015_TOTAL_SCORE1) + factor(MPED_A_BEV_NOFOOD1)
                     + factor(SMOKE_QUIT1) + factor(SMOKE_DOSE), data=data2)
 
@@ -166,12 +179,12 @@ fit.naive.lungcancer.2 <- coxph(Surv(PERSONYRS, LUNG_MORT1) ~ factor(RF_PHYS_MOD
 3657 + 8807 + 12403
 sum(aarp_data$all_mort)
 fit.naive.allcancer.1 <- coxph(Surv(PERSONYRS, all_mort) ~ factor(RF_PHYS_MODVIG_CURR1) 
-                                + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH)
+                                + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH1)
                                 + factor(BMI_CUR1) + factor(HEI2015_TOTAL_SCORE1) + factor(MPED_A_BEV_NOFOOD1)
                                 + factor(SMOKE_DOSE), data=data1)
 
 fit.naive.allcancer.2 <- coxph(Surv(PERSONYRS, all_mort) ~ factor(RF_PHYS_MODVIG_CURR1) 
-                                + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH)
+                                + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH1)
                                 + factor(BMI_CUR1) + factor(HEI2015_TOTAL_SCORE1) + factor(MPED_A_BEV_NOFOOD1)
                                 + factor(SMOKE_QUIT1) + factor(SMOKE_DOSE), data=data2)
 
@@ -179,16 +192,24 @@ fit.naive.allcancer.2 <- coxph(Surv(PERSONYRS, all_mort) ~ factor(RF_PHYS_MODVIG
 1465 + 2634 + 2972
 sum(aarp_data$respiratory_mort)
 fit.naive.RD.1 <- coxph(Surv(PERSONYRS, respiratory_mort) ~ factor(RF_PHYS_MODVIG_CURR1) 
-                        + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH)
+                        + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH1)
                         + factor(BMI_CUR1) + factor(HEI2015_TOTAL_SCORE1) + factor(MPED_A_BEV_NOFOOD1)
                         + factor(SMOKE_DOSE), data=data1)
 
 fit.naive.RD.2 <- coxph(Surv(PERSONYRS, respiratory_mort) ~ factor(RF_PHYS_MODVIG_CURR1) 
-                    + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH)
+                    + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH1)
                     + factor(BMI_CUR1) + factor(HEI2015_TOTAL_SCORE1) + factor(MPED_A_BEV_NOFOOD1)
                     + factor(SMOKE_QUIT1) + factor(SMOKE_DOSE), data=data2)
 
+fit.naive.RD.2.2 <- coxph(Surv(PERSONYRS, respiratory_mort) ~ factor(RF_PHYS_MODVIG_CURR2) 
+                        + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH1)
+                        + factor(BMI_CUR1) + factor(HEI2015_TOTAL_SCORE1) + factor(MPED_A_BEV_NOFOOD1)
+                        + factor(SMOKE_QUIT1) + factor(SMOKE_DOSE), data=data2)
 
+fit.naive.RD.2.3 <- coxph(Surv(PERSONYRS, respiratory_mort) ~ factor(RF_PHYS_MODVIG_CURR3) 
+                          + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH1)
+                          + factor(BMI_CUR1) + factor(HEI2015_TOTAL_SCORE1) + factor(MPED_A_BEV_NOFOOD1)
+                          + factor(SMOKE_QUIT1) + factor(SMOKE_DOSE), data=data2)
 ######################################################
 ## STEP 1: Suppose that there is an unobserved confounder
 ## keep in mind that we want to know at what level of alpha and beta 
@@ -198,18 +219,16 @@ fit.naive.RD.2 <- coxph(Surv(PERSONYRS, respiratory_mort) ~ factor(RF_PHYS_MODVI
 # stochastic EM with regression
 data3 <- data2[data2$RF_PHYS_MODVIG_CURR1%in%c(0,1),] # data1 have already removed category 9
 
+data3 <- data2
 
-X <- cbind(factor(data3$ENTRY_AGE1), factor(data3$SEX), factor(data3$RACEI), factor(data3$EDUCM), factor(data3$HEALTH),
+X <- cbind(factor(data3$ENTRY_AGE1), factor(data3$SEX), factor(data3$RACEI), factor(data3$EDUCM), factor(data3$HEALTH1),
            factor(data3$BMI_CUR1), factor(data3$HEI2015_TOTAL_SCORE1), factor(data3$MPED_A_BEV_NOFOOD1),
-           factor(data3$FUQ_SMOKE_STOP), factor(data3$SMOKE_DOSE))
+           factor(data3$SMOKE_QUIT1), factor(data3$SMOKE_DOSE))
 beta <- 0
 alpha <- 0
-fit.naive.RD.3 <- coxph(Surv(PERSONYRS, respiratory_mort) ~ factor(RF_PHYS_MODVIG_CURR1) 
-                        + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH)
-                        + factor(BMI_CUR1) + factor(HEI2015_TOTAL_SCORE1) + factor(MPED_A_BEV_NOFOOD1)
-                        + factor(FUQ_SMOKE_STOP) + factor(SMOKE_DOSE), data=data3)
+
 fit.stoEM_reg <- survSensitivity(data3$PERSONYRS, data3$respiratory_mort, 
-                                 data3$RF_PHYS_MODVIG_CURR1, X, "stoEM_reg",
+                                 data3$RF_PHYS_MODVIG_CURR2, X, "stoEM_reg",
                                  zetaT = beta, zetaZ = alpha,
                                  B = 5)
 theta.hat.stoEM_reg <- fit.stoEM_reg$tau1
@@ -218,19 +237,17 @@ theta.hat.stoEM_reg.se <- fit.stoEM_reg$tau1.se
 #################
 ## the proper range of alpha and beta
 # outcome ~ observed confounder
-fit.naive.RD.3 <- coxph(Surv(PERSONYRS, respiratory_mort) ~ factor(RF_PHYS_MODVIG_CURR1) 
-                        + factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH)
-                        + factor(BMI_CUR1) + factor(HEI2015_TOTAL_SCORE1) + factor(MPED_A_BEV_NOFOOD1)
-                        + factor(FUQ_SMOKE_STOP) + factor(SMOKE_DOSE), data=data3)
-fit.AW <- glm(factor(RF_PHYS_MODVIG_CURR1) ~ factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH)
+fit.naive.RD.2.3
+fit.AW <- glm(factor(RF_PHYS_MODVIG_CURR2) ~ factor(ENTRY_AGE1) + factor(SEX) + factor(RACEI) + factor(EDUCM) + factor(HEALTH1)
     + factor(BMI_CUR1) + factor(HEI2015_TOTAL_SCORE1) + factor(MPED_A_BEV_NOFOOD1)
-    + factor(FUQ_SMOKE_STOP) + factor(SMOKE_DOSE), data=data3, family = "binomial")
+    + factor(SMOKE_QUIT1) + factor(SMOKE_DOSE), data=data3, family = "binomial")
 
 # zetaT - outcome
-fit.stoEM_reg.1 <- survSensitivity(data3$PERSONYRS, data3$respiratory_mort, 
-                                 as.numeric(data3$RF_PHYS_MODVIG_CURR1)-1, X, "stoEM_reg",
-                                 zetaT = -2.5, zetaZ = -1.5,
-                                 B = 5)
+system.time(fit.stoEM_reg.1 <- survSensitivity(data3$PERSONYRS, data3$respiratory_mort, 
+                                   data3$RF_PHYS_MODVIG_CURR2, X, "stoEM_reg",
+                                 zetaT = 0.5, zetaZ = -0.5,
+                                 B = 5))
+exp(fit.stoEM_reg.1$tau1)
  ###############
 
 ######################################################
@@ -239,10 +256,9 @@ fit.stoEM_reg.1 <- survSensitivity(data3$PERSONYRS, data3$respiratory_mort,
 
 # the key is what should be a proper misclassification matrix?
 # mcsimex
-data3$SMOKE_DOSE_f <- factor(data3$SMOKE_DOSE)
-cols <- c("RF_PHYS_MODVIG_CURR1", "ENTRY_AGE1", "SEX", "RACEI", "EDUCM", "HEALTH",
+cols <- c("RF_PHYS_MODVIG_CURR2", "RF_PHYS_MODVIG_CURR3", "ENTRY_AGE1", "SEX", "RACEI", "EDUCM", "HEALTH1",
           "BMI_CUR1", "HEI2015_TOTAL_SCORE1", "MPED_A_BEV_NOFOOD1",
-          "FUQ_SMOKE_STOP", "SMOKE_DOSE")
+          "SMOKE_QUIT1", "SMOKE_DOSE")
 data3[cols] <- lapply(data3[cols], factor)
 p_ij <- matrix(c(0.8, 0.1, 0.01, 0.01, 0.01, 0.01,
                  0.16, 0.8, 0.1, 0.01, 0.01, 0.01,
@@ -251,10 +267,10 @@ p_ij <- matrix(c(0.8, 0.1, 0.01, 0.01, 0.01, 0.01,
                  0.01, 0.01, 0.01, 0.07, 0.8, 0.16,
                  0.01, 0.01, 0.01, 0.01, 0.1, 0.8), nrow = 6, byrow = TRUE)
 dimnames(p_ij) <- list(levels(data3$SMOKE_DOSE), levels(data3$SMOKE_DOSE))
-fit.naive.RD.3.t <- coxph(Surv(PERSONYRS, respiratory_mort) ~ RF_PHYS_MODVIG_CURR1
-                        + ENTRY_AGE1 + SEX +RACEI + EDUCM + HEALTH
-                        + BMI_CUR1 + HEI2015_TOTAL_SCORE1 + MPED_A_BEV_NOFOOD1
-                        + FUQ_SMOKE_STOP + SMOKE_DOSE, data=data3, model=TRUE)
+fit.naive.RD.2.2.t <- coxph(Surv(PERSONYRS, respiratory_mort) ~ RF_PHYS_MODVIG_CURR2
+                          + ENTRY_AGE1 + SEX + RACEI + EDUCM + HEALTH1
+                          + BMI_CUR1 + HEI2015_TOTAL_SCORE1 + MPED_A_BEV_NOFOOD1
+                          + SMOKE_QUIT1 + SMOKE_DOSE, data=data3, model = TRUE)
 #####################################
 # this one took really a long time to run
 ####################################
@@ -278,11 +294,11 @@ p_ij.1.1 <- matrix(c(true_prob, false_prob/2, 0, 0, 0, 0,
 dimnames(p_ij.1.1) <- list(levels(data3$SMOKE_DOSE), levels(data3$SMOKE_DOSE))
 p_ij.1.1 <- build.mc.matrix(p_ij.1.1, method = "jlt") # random
 check.mc.matrix(list(p_ij.1.1))
-fit.mcsimex.1.1 <- mcsimex(fit.naive.RD.3.t, mc.matrix = list(SMOKE_DOSE=p_ij.1.1), 
+fit.mcsimex.1.1 <- mcsimex(fit.naive.RD.2.2.t, mc.matrix = list(SMOKE_DOSE=p_ij.1.1), 
                          SIMEXvariable = c("SMOKE_DOSE"), asymptotic = FALSE)
 
 # moderate
-true_prob <- 0.7
+true_prob <- 0.65
 false_prob <- 1 - true_prob
 p_ij.1.2 <- matrix(c(true_prob, false_prob/2, 0, 0, 0, 0,
                      false_prob, true_prob, false_prob/2, 0, 0, 0,
@@ -293,11 +309,11 @@ p_ij.1.2 <- matrix(c(true_prob, false_prob/2, 0, 0, 0, 0,
 dimnames(p_ij.1.2) <- list(levels(data3$SMOKE_DOSE), levels(data3$SMOKE_DOSE))
 p_ij.1.2 <- build.mc.matrix(p_ij.1.2, method = "jlt") # not random did twice
 check.mc.matrix(list(p_ij.1.2))
-fit.mcsimex.1.2 <- mcsimex(fit.naive.RD.3.t, mc.matrix = list(SMOKE_DOSE=p_ij.1.2), 
+fit.mcsimex.1.2 <- mcsimex(fit.naive.RD.2.2.t, mc.matrix = list(SMOKE_DOSE=p_ij.1.2), 
                            SIMEXvariable = c("SMOKE_DOSE"), asymptotic = FALSE)
 
 # extreme
-true_prob <- 0.5
+true_prob <- 0.35
 false_prob <- 1 - true_prob
 p_ij.1.3 <- matrix(c(true_prob, false_prob/2, 0, 0, 0, 0,
                      false_prob, true_prob, false_prob/2, 0, 0, 0,
@@ -308,8 +324,8 @@ p_ij.1.3 <- matrix(c(true_prob, false_prob/2, 0, 0, 0, 0,
 dimnames(p_ij.1.3) <- list(levels(data3$SMOKE_DOSE), levels(data3$SMOKE_DOSE))
 p_ij.1.3 <- build.mc.matrix(p_ij.1.3, method = "jlt") # not random did twice
 check.mc.matrix(list(p_ij.1.3))
-fit.mcsimex.1.3 <- mcsimex(fit.naive.RD.3.t, mc.matrix = list(SMOKE_DOSE=p_ij.1.3), 
-                           SIMEXvariable = c("SMOKE_DOSE"), asymptotic = FALSE)
+system.time(fit.mcsimex.1.3 <- mcsimex(fit.naive.RD.2.2.t, mc.matrix = list(SMOKE_DOSE=p_ij.1.3), 
+                           SIMEXvariable = c("SMOKE_DOSE"), asymptotic = FALSE))
 
 true_prob <- 0.9
 false_prob <- 1 - true_prob
@@ -337,7 +353,25 @@ check.mc.matrix(list(p_ij.3.1))
 fit.mcsimex.3.1 <- mcsimex(fit.naive.RD.3.t, mc.matrix = list(SMOKE_DOSE=p_ij.3.1), 
                            SIMEXvariable = c("SMOKE_DOSE"), asymptotic = FALSE)
 
-
+# moderate
+true_prob <- 0.7
+false_prob <- 1 - true_prob
+p_ij.3.2 <- matrix(c(row[1],row[2:6],
+                     row[2:1],row[3:6],
+                     row[3:1],row[4:6],
+                     row[4:1],row[5:6],
+                     row[5:1],row[6],
+                     row[6:1]), nrow = 6, byrow = FALSE)
+p_ij.3.2 <- build.mc.matrix(p_ij.3.2, method = "jlt")
+check.mc.matrix(list(p_ij.3.2))
+dimnames(p_ij.3.2) <- list(levels(data3$SMOKE_DOSE), levels(data3$SMOKE_DOSE))
+system.time(fit.mcsimex.3.2 <- mcsimex(fit.naive.RD.2.2.t, mc.matrix = list(SMOKE_DOSE=p_ij.3.2), 
+                                       SIMEXvariable = c("SMOKE_DOSE"), asymptotic = FALSE))
+eigen(p_ij.1.1)
+p_ij.1.1^-0.5
+p_ij.1.1^-1
+p_ij.3.2^-0.5
+p_ij.3.2^-1
 ###########################################
 # the above two steps can be done separated
 ###########################################
